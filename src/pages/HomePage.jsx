2b0a399e48ebc6c51df3c29550fc7441b548821c
FKAYSR <kejserfreja@gmail.com>
2026-08-19 09:13:29 +0200

    update?

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <>
       <header>
-        <h1>Home Page</h1>
+        <h1>Home Page page page</h1>
       </header>
       <main>
         <p>Welcome to the home page...</p>
