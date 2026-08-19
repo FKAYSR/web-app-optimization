7f0087b6407dda9f62b105495b81315c9945abab
FKAYSR <kejserfreja@gmail.com>
2026-08-19 09:09:36 +0200

    Update header title in HomePage component to 'Home Page'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <>
       <header>
-        <h1>Home</h1>
+        <h1>Home Page</h1>
       </header>
       <main>
         <p>Welcome to the home page...</p>
